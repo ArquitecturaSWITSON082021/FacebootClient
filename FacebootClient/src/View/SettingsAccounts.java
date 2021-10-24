684df38a84cf1dc1e4189d8bea849d48bbbc4e3a
Andy0914 <urias.14@hotmail.com>
2021-10-24 02:38:21 -0700

    Agregando opción para vincular cuentas en configuración.

--- FacebootClient/src/View/SettingsAccounts.java
+++ FacebootClient/src/View/SettingsAccounts.java
@@ -11,7 +11,7 @@ import java.awt.Color;
  *
  * @author urias
  */
-public class Connections extends javax.swing.JFrame {
+public class SettingsAccounts extends javax.swing.JFrame {
 
     private int mouseX;
     private int mouseY;
@@ -19,7 +19,7 @@ public class Connections extends javax.swing.JFrame {
     /**
      * Creates new form Connections
      */
-    public Connections() {
+    public SettingsAccounts() {
         initComponents();
     }
 
@@ -303,20 +303,21 @@ public class Connections extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Connections.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SettingsAccounts.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Connections.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SettingsAccounts.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Connections.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SettingsAccounts.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Connections.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SettingsAccounts.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Connections().setVisible(true);
+                new SettingsAccounts().setVisible(true);
             }
         });
     }
